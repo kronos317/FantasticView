28f01ff6c8f8ac5a2b8d5e923d180eaf76571efb
Sahand Edrisian <sahandedrisian@Sahands-MacBook-Pro.local>
2016-09-17 19:20:36 -0700

    init

--- FantasticView/ViewController.swift
+++ FantasticView/ViewController.swift
@@ -12,14 +12,11 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let fantasticView = FantasticView(frame: self.view.bounds)
+        
+        self.view.addSubview(fantasticView)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
